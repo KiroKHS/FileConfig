061b64c771fcdef021399f013ce5e3034445c955
Nelson <66215497+KiroKHS@users.noreply.github.com>
2022-06-16 19:32:06 -0400

    Primera config qtile

---
+++ qtile/autostart.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# systray battery icon
+cbatticon -u 5 &
+# systray volume
+volumeicon &
+#fondo de pantalla
+wallapers=`ls $HOME/.config/qtile/wallpaper/`
+Fondo=(`echo $wallapers`) 
+feh --no-fehbg --bg-scale "$HOME/.config/qtile/wallpaper/${Fondo[RANDOM%${#Fondo[@]}]}" &
+#activar opacity
+compton -r 12 -o 0.00 -l 15 -t 15 -I 0.028 -O 0.03 -D 3 -c -f -C -F -G -b &
+
